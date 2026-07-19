0c4066fa4d23c6e52e31f1b0eddc624dd86ec249
anickname1 <ayxansdli557@gmail.com>
2026-07-19 13:01:10 +0400

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_spline.mk
 
@@ -10,4 +11,3 @@ COMMON_LUNCH_CHOICES := \
     omni_spline-user \
     omni_spline-userdebug \
     omni_spline-eng
-
